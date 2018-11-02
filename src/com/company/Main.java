c9b816088fe1689f9031cffa5ee5462e9d6161dc
Brendan Warshauer <bcwarshauer@gmail.com>
2018-11-02 13:07:57 -0400

    Added functionality to get weight from scale and let the user know how much of the product is left.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        Product a = new Product("Milk",10);
+        SerialTest getData = new SerialTest();
+        getData.initialize();
+        Thread t=new Thread() {
+            public void run() {
+                //the following line will keep this app alive for 1000 seconds,
+                //waiting for events to occur and responding to them (printing incoming messages to console).
+                try {Thread.sleep(1000000);} catch (InterruptedException ie) {}
+            }
+        };
+        t.start();
+        System.out.println("Started");
+
+
+        Product a = new Product("Milk",.67);
         Product b = new Product("Eggs", 14);
         Product c = new Product("Butter", 50);
 
@@ -11,13 +26,47 @@ public class Main {
 	    System.out.println(b.toString());
 	    System.out.println(c.toString());
 
-        a.setWeight(4);
+        try {
+            t.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Weight: " + getData.getSerialWeight());
+
+
+        a.setWeight(Double.parseDouble(getData.getSerialWeight()));
         b.setWeight(4);
         c.setWeight(25);
 
         System.out.println(a.toString());
-        System.out.println(b.toString());
-        System.out.println(c.toString());
+        //System.out.println(b.toString());
+        //System.out.println(c.toString());
 
+        Scanner scanner = new Scanner(System.in);
+        String answer;
+        do{
+            System.out.println("Would you like an update? (y/n)");
+            answer = scanner.next();
+
+            if(answer.equals("y")){
+                try {
+                    t.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Weight: " + getData.getSerialWeight());
+                a.setWeight(Double.parseDouble(getData.getSerialWeight()));
+                b.setWeight(4);
+                c.setWeight(25);
+
+                System.out.println(a.toString());
+                //System.out.println(b.toString());
+                //System.out.println(c.toString());
+            }
+
+        }while(answer.equals("y"));
+
+    System.out.println("Terminate");
+    System.exit(0);
     }
 }
